1c28aa2a1cb2a966f93715c303b79bc97ac93c74
nxstray <afwanapriansyah@gmail.com>
2026-06-29 21:29:36 +0700

    feat(config): setup vite proxy for /api and /uploads endpoints with server port 5173

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
+import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [
@@ -13,7 +12,20 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
   },
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8020',
+        changeOrigin: true
+      },
+      '/uploads': {
+        target: 'http://localhost:8020',
+        changeOrigin: true
+      }
+    }
+  }
 })
