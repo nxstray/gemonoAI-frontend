4655e8f63ae00477736d14c823b4ed49705986b6
nxstray <afwanapriansyah@gmail.com>
2026-07-03 16:17:45 +0700

    feat(ui): integrate GooeyToaster notification system in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
+  <GooeyToaster position="top-center" />
   <RouterView />
 </template>
 
 <script setup>
 import { RouterView } from 'vue-router'
+import { GooeyToaster } from 'vue-goey-toast'
+import 'vue-goey-toast/styles.css'
 </script>
