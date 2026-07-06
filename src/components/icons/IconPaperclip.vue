73967cfcf977e27dbe5f6524bb0e8004531d3b6f
nxstray <afwanapriansyah@gmail.com>
2026-07-06 23:37:51 +0700

    feat(component): add Paperclip icon component

---
+++ src/components/icons/IconPaperclip.vue
@@ -0,0 +1,6 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"
+       fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+    <path d="M21.44 11.05l-9.19 9.19a6 6 0 0 1-8.49-8.49l9.19-9.19a4 4 0 0 1 5.66 5.66L9.41 17.41a2 2 0 0 1-2.83-2.83l8.49-8.48"/>
+  </svg>
+</template>
